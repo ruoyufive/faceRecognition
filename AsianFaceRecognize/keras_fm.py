a0378869cd5a0f52e57cd6e60c476d887981269a
Muyun <muyun@nuaa.edu.cn>
2019-01-24 17:28:26 +0800

    Update Code

--- AsianFaceRecognize/keras_fm.py
+++ AsianFaceRecognize/keras_fm.py
@@ -6,6 +6,8 @@ import numpy as np
 from PIL import Image
 import os
 from matplotlib import pyplot as plt
+from keras.utils import plot_model
+import random
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -16,11 +18,13 @@ from keras import backend as K
 
 img_rows = 250  # 图像行数
 img_cols = 250  # 图像列数
-batchSize =1000  # 批处理数量
+batchSize = 514  # 批处理数量
 epochs = 40  # 训练次数
 
-historyName = './TestPics/Plt/history_1.png'
-modelWeightName = './TestPics/Plt/modelweight.h5'
+historyName = './TestPics/Plt/history_2.png'
+historyTitle = 'Model accuracy'
+modelPlotName = './TestPics/Plt/model_2.png'
+modelWeightName = './TestPics/Plt/modelweight_2.h5'
 
 # 性别字典
 sexDict = {'AF': 0, 'AM': 1}
@@ -34,10 +38,8 @@ headDirDict = {'00F': 90, '30L': 60, '30R': 120,
 
 def show_train_data(history, hisPath):
     plt.plot(history.history['loss'])
-    # print(history.history["loss"])
     plt.plot(history.history['val_loss'])
-    # print(history.history.keys())
-    plt.title('Model accuracy')
+    plt.title(historyTitle)
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Valid'], loc='upper left')
@@ -45,8 +47,15 @@ def show_train_data(history, hisPath):
     plt.show()
 
 
+def show_model(model, modelPlotName=modelPlotName):
+    plot_model(model, to_file=modelPlotName)
+
+
 def load_data(dirPath):
     files = os.listdir(dirPath)
+    print('Before ', files)
+    random.shuffle(files)
+    print('After ', files)
     # print('listing ', dirPath)
     picNum = len(files)
 
@@ -86,7 +95,7 @@ def load_data(dirPath):
     return (data, label)
 
 
-def set_model(learnRate=0.005, decay=1e-6, momentum=0.9):
+def set_model(learnRate=0.005, decay=1e-6, momentum=0.5):
     model = Sequential()
     if K.image_data_format() == 'channels_first':
         model.add(Conv2D(5, kernel_size=(3, 3), input_shape=(1, img_rows, img_cols)))
@@ -96,30 +105,26 @@ def set_model(learnRate=0.005, decay=1e-6, momentum=0.9):
     model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    # model.add(Conv2D(32, kernel_size=(3, 3)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    #
-    # model.add(Conv2D(64, kernel_size=(3, 3)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(5, kernel_size=(3, 3)))
+    model.add(Conv2D(10, kernel_size=(3, 3)))
     model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(3, 3)))
 
-    model.add(Dropout(0.1))
-    model.add(Flatten())
-    model.add(Dense(128))  # Full connection
+    # -------------------------------------------------------------
+    model.add(Dropout(0.25))
 
-    model.add(Activation('tanh'))
-    model.add(Dropout(0.1))
+    model.add(Flatten())
+    model.add(Dense(1000))  # Full connection
+
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
     model.add(Dense(2))
     model.add(Activation('softmax'))
 
-
     sgd = SGD(lr=learnRate, decay=decay, momentum=momentum, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+    show_model(model, modelPlotName)
     return model
 
 
@@ -127,21 +132,23 @@ def train_model(model, X_train, Y_train, X_val, Y_val, molWeightPath):
     history = model.fit(X_train, Y_train, batch_size=batchSize, epochs=epochs,
                         verbose=1, validation_data=(X_val, Y_val))
     model.save_weights(molWeightPath, overwrite=True)
-    #show_train_data(history, historyName)
+    show_train_data(history, historyName)
     return model
 
 
 def test_model(model, X, Y, modelPath):
     model.load_weights(modelPath)
     score = model.evaluate(X, Y, verbose=0)
+    print('model summary ',model.summary())
     return score
 
 
 if __name__ == '__main__':
     # 加载数据集
-    # (X_train, y_train) = load_data('./TestPics/TrainPics')
     (X_train, y_train) = load_data('./TestPics/TrainPics')
     (X_valid, y_valid) = load_data('./TestPics/ValidPics')
+    # X_sum, y_sum = load_data('./TestPics/ValidPics')
+    # X_train, X_valid, y_train, y_valid = train_test_split(X_sum, y_sum, test_size=0.125, random_state=42)
     (X_test, y_test) = load_data('./TestPics/Test')
 
     # print(X_train)
@@ -172,10 +179,12 @@ if __name__ == '__main__':
     model = set_model()
     train_model(model, X_train, Y_train, X_valid, Y_valid, modelWeightName)
     score = test_model(model, X_test, Y_test, modelWeightName)
+    show_model(model, modelPlotName)
     print('score : ', score)
 
     model.load_weights(modelWeightName)
     classes = model.predict_classes(X_test, verbose=0)
+
     accuracy = np.mean(np.equal(y_test, classes))
     print('accuarcy : ', accuracy)
     for index in range(len(y_test)):
